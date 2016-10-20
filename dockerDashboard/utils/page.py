07c5a72f39246b99bc978716b0dd8fe8bcaa0156
Tom .Lee <1123431949@qq.com>
2016-10-20 14:47:40 +0800

    Update page.py

--- dockerDashboard/utils/page.py
+++ dockerDashboard/utils/page.py
@@ -18,7 +18,9 @@ def pagination(request, queryset, display_amount=10, after_range_num=5, bevor_ra
         objects = paginator.page(paginator.num_pages)
     except:
         objects = paginator.page(1)
-
+    
+    page = objects.number
+    
     if page >= after_range_num:
         page_range = paginator.page_range[page - after_range_num:page + bevor_range_num]
     else:
